761e642a78ec77a1e9fbe64ed55f73d56f6c873e
Max Kaemmerer <maxkaemmerer.hb@gmail.com>
2025-04-05 17:33:48 +0200

    - kinda got walking sounds working

--- .build/index.service.worker.js
+++ .build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1743863425|6469925734';
+const CACHE_VERSION = '1743867212|10257404536';
 /** @type {string} */
 const CACHE_PREFIX = 'ld57-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
--- scenes/player.gd
+++ scenes/player.gd
@@ -3,19 +3,20 @@ extends CharacterBody2D
 enum PlayerId {P1 = 1, P2 = 2}
 
 const SPEED = 150.0
+
 @export var player_id: PlayerId
+
+@onready var player_walking = $PlayerWalking
 
 var ping
 
 func _ready() -> void:
 	ping = preload("res://scenes/ping.tscn")
-
-
-func _physics_process(delta: float) -> void:	
-	velocity = Vector2(0, 0)
+func _physics_process(delta: float) -> void:
 	
+	# Handle Input
+	velocity = Vector2.ZERO
 	var player_prefix: String = "p" + str(player_id)
-	
 	if Input.is_action_pressed(player_prefix + "-down"):
 		velocity.y += SPEED
 	if Input.is_action_pressed(player_prefix + "-up"):
@@ -28,7 +29,13 @@ func _physics_process(delta: float) -> void:
 		var ping_instance = ping.instantiate()
 		ping_instance.position = position
 		add_sibling(ping_instance)
+	
+	if velocity == Vector2.ZERO: 
+		player_walking.stop()
+	elif !player_walking.playing:
+		player_walking.play()
 		
+	# Handle Collusion
 	var collision_info = move_and_collide(velocity * delta)
 	if collision_info:
 			print(collision_info)

9cb0e5c14663ddcf7919bfb7fee810685b377270
Max Kaemmerer <maxkaemmerer.hb@gmail.com>
2025-04-05 15:15:55 +0200

    - add basic floors - fix z indexinh

--- .build/index.service.worker.js
+++ .build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1743858202|1247503702';
+const CACHE_VERSION = '1743858925|1970212795';
 /** @type {string} */
 const CACHE_PREFIX = 'ld57-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

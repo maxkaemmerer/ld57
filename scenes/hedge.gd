1cd67107b52921288d35e042ddc748eec63740eb
Max Kaemmerer <maxkaemmerer.hb@gmail.com>
2025-04-05 13:30:04 +0200

    - mute audio  - adjust hedge rotation

--- scenes/hedge.gd
+++ scenes/hedge.gd
@@ -1,7 +1,5 @@
 extends StaticBody2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var sprite: Sprite2D = self.get_node("Sprite2D")
+	var sprite: Sprite2D = self.get_node("./Sprite2D")
 	sprite.rotate(deg_to_rad(randi_range(1, 4) * 90))

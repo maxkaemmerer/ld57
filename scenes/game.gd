255d1f8fc631aaed7f583cf4302bc618223f444f
Max Kaemmerer <maxkaemmerer.hb@gmail.com>
2025-04-05 14:01:03 +0200

    - split screen

---
+++ scenes/game.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@onready var viewport1 = $Viewports/P1/SubViewport
+@onready var viewport2 = $Viewports/P2/SubViewport
+@onready var camera1 = $Viewports/P1/SubViewport/Camera2D
+@onready var camera2 = $Viewports/P2/SubViewport/Camera2D
+@onready var world = $Viewports/P1/SubViewport/Level
+
+
+func _ready():
+	viewport2.world_2d = viewport1.world_2d
+	camera1.target = world.get_node("Player_1")
+	camera2.target = world.get_node("Player_2")

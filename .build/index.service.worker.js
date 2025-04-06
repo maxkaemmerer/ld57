edf13fa94dcf7dea02c1f4917520440242f22a90
Max Kaemmerer <maxkaemmerer.hb@gmail.com>
2025-04-06 18:48:14 +0200

    -  build

--- .build/index.service.worker.js
+++ .build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1743944202|25786293290';
+const CACHE_VERSION = '1743958082|39665820731';
 /** @type {string} */
 const CACHE_PREFIX = 'ld57-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

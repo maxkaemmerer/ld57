30f32295261b1069f30747daa89fbca2e5e7b54b
Max Kaemmerer <maxkaemmerer.hb@gmail.com>
2025-04-07 15:25:10 +0200

    - new fixed build

--- .build/web/index.service.worker.js
+++ .build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1744028414|538249711';
+const CACHE_VERSION = '1744031912|88783015';
 /** @type {string} */
 const CACHE_PREFIX = 'ld57-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
